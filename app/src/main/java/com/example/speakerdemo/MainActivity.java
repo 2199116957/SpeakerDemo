106993fba21dc7840457644eaa65f3c5202e213e
unknown <ljh18519142135@163.com>
2021-05-18 19:49:44 +0800

    首次提交

---
+++ app/src/main/java/com/example/speakerdemo/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.speakerdemo;
+
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.speakerdemo.fragment.curriculum.CurriculumFragment;
+import com.example.speakerdemo.fragment.home.HomeFragment;
+import com.example.speakerdemo.fragment.me.MeFragment;
+import com.example.speakerdemo.fragment.more.MoreFragment;
+import com.example.speakerdemo.fragment.news.NewsFragment;
+import com.google.android.material.tabs.TabLayout;
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout fragmentMain;
+    private TabLayout tablayoutMain;
+    private FragmentManager supportFragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+    /*
+    *设置页面切换
+    * */
+    private void initView() {
+        //获取界面控件ID
+        fragmentMain = (FrameLayout) findViewById(R.id.fragment_Main);
+        tablayoutMain = (TabLayout) findViewById(R.id.tablayout_Main);
+
+        //获取Fragment对象
+        //首页fragment
+        HomeFragment homeFragment = new HomeFragment();
+        //课程fragment
+        CurriculumFragment curriculumFragment = new CurriculumFragment();
+        //更多fragment
+        NewsFragment newsFragment = new NewsFragment();
+        //消息fragment
+        MoreFragment moreFragment = new MoreFragment();
+        //我的fragment
+        MeFragment meFragment = new MeFragment();
+        //获取supportFragmentManager对象，把fragment对象添加到supportFragmentManager中
+        supportFragmentManager = getSupportFragmentManager();
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_Main, homeFragment)
+                .add(R.id.fragment_Main, curriculumFragment)
+                .add(R.id.fragment_Main, newsFragment)
+                .add(R.id.fragment_Main, moreFragment)
+                .add(R.id.fragment_Main, meFragment)
+                .show(homeFragment)
+                .hide(curriculumFragment)
+                .hide(moreFragment)
+                .hide(newsFragment)
+                .hide(meFragment)
+                .commit();
+        //添加TabLayout选择器和文字
+        tablayoutMain.addTab(tablayoutMain.newTab().setText(R.string.main_app_Home).setIcon(R.drawable.main_tablayout_home_select));
+        tablayoutMain.addTab(tablayoutMain.newTab().setText(R.string.main_app_Curriculum).setIcon(R.drawable.main_tablayout_curriculum_select));
+        tablayoutMain.addTab(tablayoutMain.newTab().setText(R.string.main_app_news).setIcon(R.drawable.main_tablayout_news_select));
+        tablayoutMain.addTab(tablayoutMain.newTab().setText(R.string.main_app_more).setIcon(R.drawable.main_tablayout_more_select));
+        tablayoutMain.addTab(tablayoutMain.newTab().setText(R.string.main_app_Me).setIcon(R.drawable.main_tablayout_me_select));
+        tablayoutMain.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //根据TabLayout切换页面，show显示当前fragment，hide隐藏fragment
+                switch (tab.getPosition()) {
+                    case 0:
+                        supportFragmentManager.beginTransaction()
+                                .show(homeFragment)
+                                .hide(curriculumFragment)
+                                .hide(moreFragment)
+                                .hide(newsFragment)
+                                .hide(meFragment)
+                                .commit();
+                        break;
+                    case 1:
+                        supportFragmentManager.beginTransaction()
+                                .show(curriculumFragment)
+                                .hide(homeFragment)
+                                .hide(moreFragment)
+                                .hide(newsFragment)
+                                .hide(meFragment)
+                                .commit();
+                        break;
+                    case 2:
+                        supportFragmentManager.beginTransaction()
+                                .show(newsFragment)
+                                .hide(curriculumFragment)
+                                .hide(homeFragment)
+                                .hide(moreFragment)
+                                .hide(meFragment)
+                                .commit();
+                        break;
+                    case 3:
+                        supportFragmentManager.beginTransaction()
+                                .show(moreFragment)
+                                .hide(curriculumFragment)
+                                .hide(newsFragment)
+                                .hide(homeFragment)
+                                .hide(meFragment)
+                                .commit();
+                        break;
+                    case 4:
+                        supportFragmentManager.beginTransaction()
+                                .show(meFragment)
+                                .hide(curriculumFragment)
+                                .hide(moreFragment)
+                                .hide(newsFragment)
+                                .hide(homeFragment)
+                                .commit();
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/speakerdemo/fragment/curriculum/CurriculumFragment.java
@@ -0,0 +1,21 @@
+package com.example.speakerdemo.fragment.curriculum;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.speakerdemo.R;
+
+
+public class CurriculumFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_curriculum, container, false);
+    }
+}
